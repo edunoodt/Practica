da5d2445c6654d12a97aa07b5d8827390d93e38a
edunoodt <edunoodt@yahoo.com.ar>
2022-10-17 22:43:44 -0300

    Introduccion a objetos

---
+++ objetos 01.py
@@ -0,0 +1,31 @@
+from os import system
+import random
+
+system('cls')
+
+class movil():
+    def __init__(self,nombre):
+        self.nombre = nombre
+        self.posicion = 0
+
+    def avanza(self):
+        self.posicion += random.randint(1,4)
+
+    def __str__(self):
+        print (self.nombre,' - posicion: ',str(self.posicion))
+
+
+nombre1 = input ('ingrese el nombre del primer movil: ')
+nombre2 = input ('ingrese el nombre del segundo movil: ')
+
+
+movil01 = movil(nombre1)
+movil02 = movil(nombre2)
+
+for ciclos in range (10):
+    print (movil01.__str__())
+    print (movil02.__str__())
+    movil01.avanza()
+    movil02.avanza()
+
+
